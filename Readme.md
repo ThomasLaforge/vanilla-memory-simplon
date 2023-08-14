88fa9e38dbdb3f491951971d629a6d33c7a86cf2
ThomasLaforge <thomas.laforge.38@gmail.com>
2023-08-15 00:18:16 +0200

    memory with colors

--- Readme.md
+++ Readme.md
@@ -17,6 +17,9 @@ Utiliser le Javascript dans le navigateur pour intéragir avec le DOM et l'utili
 
 ## Instructions
 
+- Vous réaliserez ce travail par groupes de 2 développeurs
+- Travaillez sur un repository github partagé
+- Créer un wireframe pour le jeu
 - Créer un nouveau projet avec vite en Vanilla + Typescript
 ```bash
 npm init vite@latest
@@ -31,14 +34,96 @@ npm init vite@latest
     - Si les 2 cartes sont identiques, les laisser retournées
     - Si les 2 cartes sont différentes, les retourner de nouveau
     - Si toutes les cartes sont retournées, afficher un message de victoire
+- Mise en ligne sur Vercel
+    - Créer un compte sur Vercel
+    - Lier le projet github
+    - Déployer le projet
+    - Tester le jeu en ligne
 
 ## Bonus
 
 A la place des couleurs sur les cartes, afficher des images récupérées depuis une API.
 https://dog.ceo/api/breeds/image/random
 
+## Rendu
+
+- Lien vers le repository github
+- Lien vers le jeu en ligne (lien Vercel)
+
 ## Ressources
 
+```html
+<body>
+    <h1>Memory</h1>
+    <button id="init-button">Commencer une nouvelle partie</button>
+    <div id="app"></div>
+    <img id="image-chat" alt="image de chien">
+    <script type="module" src="/src/main.ts"></script>
+</body>
+```
 
+```ts
+const colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown"];
 
-https://dog.ceo/dog-api/
+// Attention aux "as" qui règlent pas mal de soucis
+const app = document.querySelector('#app') as HTMLDivElement;
+const btnStart = document.querySelector("#init-button") as HTMLButtonElement;
+
+let cpt = 0;
+
+// Promises
+fetch("https://dog.ceo/api/breeds/image/random")
+.then((response) => {
+    return response.json();
+})
+.then((data) => {
+    console.log(data);
+    document.querySelector("#image-chat")?.setAttribute("src", data.message);
+})
+
+// Add an event listener
+btnStart.addEventListener("click", () => {
+    init();
+});
+
+function init(){
+    console.log('init')
+    btnStart.remove();
+    cpt++;
+    app.innerHTML = `
+        <p>${cpt}</p>
+        <button id="init-button">Nouveau Click</button>
+    `;
+
+    const tiles = new Array(16).fill('').map( (_, i) => {
+        const tile = document.createElement("div")
+        tile.setAttribute("class", "tile")
+        tile.style.width ="50px"
+        tile.style.height = "50px"
+        tile.style.backgroundColor = colors[Math.floor(i/2)]
+        return tile
+    })
+    // Shuffle the tiles
+    tiles.sort( () => Math.random() - 0.5)
+    
+    // Add the tiles to the app
+    tiles.forEach( tile => app.appendChild(tile))
+
+    let nodeList = document.querySelectorAll(".tile");
+    let elements = Array.from(nodeList);
+    elements.forEach( (element) => {
+        element.setAttribute("class", "not-revvealed")
+        element.addEventListener("click", () => {
+            element.setAttribute("class", "revealed")
+        })
+    })
+    
+    // Add an event listener
+    const newBtnStart = document.querySelector("#init-button") as HTMLButtonElement;
+    newBtnStart.addEventListener("click", () => {
+        init();
+    });
+}
+
+```
+- https://dog.ceo/dog-api/
--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,40 @@
+let timeoutId: number;
 const colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown"];
 
 // Attention aux "as" qui règlent pas mal de soucis
 const app = document.querySelector('#app') as HTMLDivElement;
 const btnStart = document.querySelector("#init-button") as HTMLButtonElement;
 
-let cpt = 0;
+const tilesDOM = document.createElement('div') as HTMLDivElement;
+tilesDOM.classList.add('tiles');
+app.append(tilesDOM)
 
 // Promises
-fetch("https://dog.ceo/api/breeds/image/random")
-.then((response) => {
-    return response.json();
-})
-.then((data) => {
-    console.log(data);
-    document.querySelector("#image-chat")?.setAttribute("src", data.message);
-})
+// fetch("https://dog.ceo/api/breeds/image/random")
+// .then((response) => {
+//     return response.json();
+// })
+// .then((data) => {
+//     console.log(data);
+//     document.querySelector("#image-chat")?.setAttribute("src", data.message);
+// })
 
 // Add an event listener
-btnStart.addEventListener("click", () => {
+btnStart?.addEventListener("click", () => {
     init();
 });
 
 function init(){
     console.log('init')
     btnStart.remove();
-    cpt++;
-    app.innerHTML = `
-        <p>${cpt}</p>
-        <button id="init-button">Nouveau Click</button>
-    `;
+    document.querySelectorAll(".tile").forEach( (element) => {
+        element.remove();
+    })
 
     const tiles = new Array(16).fill('').map( (_, i) => {
         const tile = document.createElement("div")
-        tile.setAttribute("class", "tile")
-        tile.style.width ="50px"
-        tile.style.height = "50px"
-        tile.style.backgroundColor = colors[Math.floor(i/2)]
+        tile.classList.add("tile")
+        tile.classList.add(colors[Math.floor(i / 2)])
         return tile
     })
     // Shuffle the tiles
@@ -44,9 +43,59 @@ function init(){
     // Add the tiles to the app
     tiles.forEach( tile => app.appendChild(tile))
 
-    // Add an event listener
-    const newBtnStart = document.querySelector("#init-button") as HTMLButtonElement;
-    newBtnStart.addEventListener("click", () => {
-        init();
-    });
+    // Select multiple elements
+    let nodeList = document.querySelectorAll(".tile");
+    let elements = Array.from(nodeList);
+    elements.forEach( (element) => {
+        element.classList.add("not-revealed")
+        
+        element.addEventListener("click", () => {
+            if(element.classList.contains("revealed")){
+                element.classList.remove("revealed")
+                element.classList.add("not-revealed")
+            } else {
+                let revealed = document.querySelectorAll(".revealed")
+
+                if(revealed.length === 2){
+                    clearTimeout(timeoutId)
+                    document.querySelectorAll(".revealed").forEach( (element) => {
+                        element.classList.remove("revealed")
+                        element.classList.add("not-revealed")
+                    })
+                }
+
+                element.classList.remove("not-revealed")
+                element.classList.add("revealed")
+                revealed = document.querySelectorAll(".revealed")
+                
+                if(revealed.length === 2){
+                    if(revealed[0].classList.item(1) === revealed[1].classList.item(1)){   
+                        const first = revealed[0]
+                        const second = revealed[1]
+
+                        if(first.classList.item(1) === second.classList.item(1)){
+                            first.classList.remove("revealed")
+                            first.classList.add("found")
+                            second.classList.remove("revealed")
+                            second.classList.add("found")
+                        }
+                    } else {
+                        timeoutId = setTimeout(() => {
+                            document.querySelectorAll(".revealed").forEach( (element) => {
+                                element.classList.remove("revealed")
+                                element.classList.add("not-revealed")
+                            })
+                        }, 1000)
+                    }
+                }
+            }
+
+            if(document.querySelectorAll(".found").length === 16){
+                setTimeout( () => alert("Bravo ! Partie terminée"), 100)
+                app.appendChild(btnStart)
+            }
+        })
+        
+        tilesDOM.appendChild(element)
+    })
 }
